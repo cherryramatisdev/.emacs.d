01e73c8208ff01ef21f191b14a92d0ae60b0f6fd
CherryRamatis <cherry.ramatis@lamimed.it>
2022-05-17 08:18:26 -0300

    feat: add custom leader to abstract C-c bindings

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -10,5 +10,14 @@
 (define-key my-leader (kbd "c") 'compile)
 (define-key my-leader (kbd "r") 'recompile)
 
+(define-key my-leader (kbd "p") nil)
+(define-key my-leader (kbd "f") 'project-find-file)
+(define-key my-leader (kbd "pp") 'project-switch-project)
+(define-key my-leader (kbd "pc") 'project-compile)
+(define-key my-leader (kbd "pe") 'project-eshell)
+
+(define-key xah-fly-command-map (kbd "<backspace>") my-leader
+)
+
 (provide 'keybindings)
 ;;; keybindings.el ends here
