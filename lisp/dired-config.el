c39a2b1820fb3851c9a36294e8ec995751d559ef
CherryRamatis <cherry.ramatis@lamimed.it>
2022-05-24 20:37:19 -0300

    feat: add a custom function to rename file.

--- lisp/dired-config.el
+++ lisp/dired-config.el
@@ -7,5 +7,12 @@
 (define-key dired-mode-map (kbd "C-b") 'dired-up-directory)
 (define-key dired-mode-map (kbd "C-f") 'dired-find-file)
 
+(defun my/rename-file ()
+  (interactive)
+  (rename-file (thing-at-point 'word 'no-properties) (read-string "Insert new name: "))
+  (call-interactively 'revert-buffer))
+
+(define-key dired-mode-map (kbd "R") 'my/rename-file)
+
 (provide 'dired-config)
 ;;; dired-config.el ends here
