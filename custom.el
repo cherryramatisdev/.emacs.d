c248040eeec280d2a2e7ea1aac98c560d8fa152b
Cherry Ramatis <cherry.ramatis@lamimed.it>
2022-06-24 10:47:02 -0300

    feat: install markdown mode to use sometimes

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(elixir-mode xah-fly-keys web-mode use-package typescript-mode tempel perspective orderless naysayer-theme magit corfu cider ag)))
+   '(markdown-mode elixir-mode xah-fly-keys web-mode use-package typescript-mode tempel perspective orderless naysayer-theme magit corfu cider ag)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
